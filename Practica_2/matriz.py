cb7be574d02e3c125d296c693f1e560f6daed3f9
Alfonso Valadez <alfonso.2000.ava@gmail.com>
2026-02-18 08:52:20 -0600

    INSERT Ejercicio Matriz

---
+++ Practica_2/matriz.py
@@ -0,0 +1,11 @@
+A = [[5, 6, 13], [3, 10, 1], [2, 11, 3]]
+B = [[1, 2, 17], [6, 5, 15], [3, 11, 12]]
+AB =[[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+
+for i in range(len(A)):
+    for j in range(len(B[0])):   
+        for k in range(len(B)):
+            AB[i][j] += A[i][k] * B[k][j]
+
+print(AB)
